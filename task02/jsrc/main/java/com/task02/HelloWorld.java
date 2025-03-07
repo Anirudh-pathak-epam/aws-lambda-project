53c02d58626a00ad6dd9389518a540a9e0826a3f
Anirudh Pathak <anirudh_pathak@epam.com>
2025-03-07 15:32:58 +0530

    fixed

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,39 +3,25 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.events.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
     lambdaName = "hello_world",
-    roleName = "hello_world-role",
-    isPublishVersion = true,
-    aliasName = "${lambdas_alias_name}",
-    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+	roleName = "hello_world-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(authType = AuthType.NONE) // Enables Function URL with public access
-public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
-
-    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-        String path = (String) event.getOrDefault("rawPath", "");
-        Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
-        Map<String, Object> http = (requestContext != null) ? (Map<String, Object>) requestContext.get("http") : null;
-        String method = (http != null) ? (String) http.get("method") : "";
-
-        Map<String, Object> response = new HashMap<>();
-
-        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-            response.put("statusCode", 200);
-            response.put("message", "Hello from Lambda");
-        } else {
-            response.put("statusCode", 400);
-            response.put("message", "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
-        }
-
-        return response;
-    }
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+	@LambdaUrlConfig(authType = AuthType.NONE)
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "Hello from Lambda");
+		return resultMap;
+	}
 }
